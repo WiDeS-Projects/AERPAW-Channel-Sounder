22e019e826e412dc6bbc48f283e1b953bc2f4b5d
BrianZ <yzhang26@usc.edu>
2025-01-22 21:49:32 -0800

    Read to test on emulator for 1drone-3BS
    1. Added real and local simulation yaml files
    2. Modified the library issue in the setup.py file

--- LW3/sounder/setup.py
+++ LW3/sounder/setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-package = Extension('ChsRX', ['ChsRX.pyx'], include_dirs=[numpy.get_include()])
+package = Extension('ChsRX', ['ChsRX.pyx'], include_dirs=[numpy.get_include(),"/usr/local/lib/python3.10/dist-packages/numpy/core/include/"])
 setup(ext_modules=cythonize([package]))
