d3dbb53e8bf44c27dd4cc95aee076a59e0060185
BrianZ <yzhang26@usc.edu>
2025-01-22 22:19:14 -0800

    changed the way to include the library

--- LPN1/run.sh
+++ LPN1/run.sh
@@ -4,3 +4,4 @@ pip3 install --upgrade scipy
 pip3 install --upgrade numpy
 pip3 install --upgrade pybind11
 pip3 install numpy==2.1
+cd sounder
--- LW2/run.sh
+++ LW2/run.sh
@@ -4,3 +4,5 @@ pip3 install --upgrade scipy
 pip3 install --upgrade numpy
 pip3 install --upgrade pybind11
 pip3 install numpy==2.1
+cd sounder
+export CFLAGS=-I/usr/local/lib/python3.10/dist-packages/numpy/_core/include/
