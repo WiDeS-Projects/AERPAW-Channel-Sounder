f64c8a7ce6b84115e82a7d7a632b1cac1db737d8
BrianZ <yzhang26@usc.edu>
2025-01-22 22:47:43 -0800

    Combine all LW nodes into Rx; LPN1 into Tx
    will inlcude the python library using commands

--- LPN1/run.sh
+++
@@ -1,7 +0,0 @@
-pip3 install -r requirements.txt
-cd sounder
-pip3 install --upgrade scipy
-pip3 install --upgrade numpy
-pip3 install --upgrade pybind11
-pip3 install numpy==2.1
-cd sounder
--- LW3/run.sh
+++
@@ -1,8 +0,0 @@
-pip3 install -r requirements.txt
-cd sounder
-pip3 install --upgrade scipy
-pip3 install --upgrade numpy
-pip3 install --upgrade pybind11
-pip3 install numpy==2.1
-cd sounder
-export CFLAGS=-I/usr/local/lib/python3.10/dist-packages/numpy/_core/include/
